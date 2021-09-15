c1a563071d009647ff336271b3380c4ec34cef00
Yandzick <yandzick@gmail.com>
2021-09-15 19:02:28 -0300

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,23 @@
+public class Time {
+
+    //atributos
+    private String nome;
+    private String escudo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEscudo() {
+        return escudo;
+    }
+
+    public void setEscudo(String escudo) {
+        this.escudo = escudo;
+    }
+    
+}
